cf3d6367c44bfcfe27d80348f2545f9c053915fd
ikariLain <matheus-torrico@outlook.com>
2025-01-12 00:42:48 +0100

    Add solution labb1

---
+++ Labb1/MyFirstProgram.py
@@ -0,0 +1,14 @@
+# variable with a if statement
+number = 9
+if number > 10: 
+    print("Number is greater than 10")
+else:
+    print("Number is not greater than 10")
+
+# 
+name = input("Enter your name:")
+print(f"Hi {name}!")
+
+# Example of for-loop
+for i in range(number):
+    print(i)    
