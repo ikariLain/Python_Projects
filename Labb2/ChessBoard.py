2476427d5cc82d64a2657f1f05f9682853494d47
ikariLain <matheus-torrico@outlook.com>
2025-01-12 10:45:18 +0100

    Add method and switch

--- Labb2/ChessBoard.py
+++ Labb2/ChessBoard.py
@@ -1,10 +1,32 @@
-print("Welcome to the chessBoard")
-size = input("Enter board size:")
-int_size = int(size)
-for i in range(int_size):
+
+def EasyMethod():
+    print("Welcome to the chessBoard")
+    size = input("Enter board size:")
+    int_size = int(size)
+    for i in range(int_size):
+        print()
+        for j in range(int_size):
+            if (i + j) % 2 == 0:
+                print("S", end="")
+            else:
+                print("W", end="")
     print()
-    for j in range(int_size):
-        if (i + j) % 2 == 0:
-            print("S", end="")
-        else:
-            print("W", end="")
+
+    
+def harderMethod():
+    print("Welcome to the hard")
+
+switch = {
+    "1" : EasyMethod,
+    "2" : harderMethod
+}   
+print("Enter you what kind of cheesboard?")
+print("1. Easy method")
+print("2. Hard method")
+value = input()
+
+selected_method = switch.get(value,lambda : "invalid option")
+result = selected_method()
+
+if result:
+    print(result)
