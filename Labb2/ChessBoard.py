ac51acc5867228e93bd4e08ea83de3ced22b702f
ikariLain <matheus-torrico@outlook.com>
2025-01-12 16:12:39 +0100

    finish harder method

--- Labb2/ChessBoard.py
+++ Labb2/ChessBoard.py
@@ -7,33 +7,68 @@ def EasyMethod():
         print()
         for j in range(int_size):
             if (i + j) % 2 == 0:
-                print("S", end="")
+                print("W", end="")
             else:
                 print("B", end="")
     print()
 
+def Print_grid(grid):
+        for row in grid:
+            print(" ".join(row)) 
     
 def harderMethod():
+    
     print("Welcome to the harder method")
     print("Press max size 64")
     size = input("Enter board size:")
+    
+    
+    
     if int(size) > 64:
         print("Size too large, max size 64")
         return
-    else:
-        symbol1 = input("Enter symbol for white pieces: ")
-        symbol2 = input("Enter symbol for black pieces: ")
-        int_size = int(size)
-        for i in range(int_size):
-            print()
-            for j in range(int_size):
-                if (i + j) % 2 == 0:
-                    print(f"{symbol1}", end="")
-                else:
-                    print(f"{symbol2}", end="")
-    print()
+
+    # Get the needed symbols from user
+    symbol1 = input("Enter symbol for white pieces: ")
+    symbol2 = input("\nEnter symbol for black pieces: ")
+    int_size = int(size)
     
     
+    grid = [[""for _ in range(int_size)] for _ in range(int_size)]
+    for i in range(int_size):
+        for j in range(int_size):
+                grid[i][j] = symbol1 if(i+j) % 2 == 0 else symbol2
+                
+    #Print out grid
+    print("\nInitial Chessboard")
+    Print_grid(grid)
+    
+    # Allow to make custom symbols dynamically
+    while True:
+        print("\nWould you like to modify the chessboard? [Y/N]")
+        choice = input().lower()
+        if choice == "n":
+            break
+        
+        # Get coordinates    
+        x = int(input("Enter x coordinate (row):  "))
+        y = int(input("Enter y coordinate (column): "))
+        custom_symbol = input("Enter symbol: ")
+    
+        if 0 <= x < int_size and 0 <= y < int_size:
+            grid[x][y] = custom_symbol
+        else:
+            print("Invalid coordinates")
+        
+        print("\nUpdated chessboard:")
+        Print_grid(grid)
+        
+    print("\nFinal Chessboard")
+    Print_grid(grid)
+        
+           
+        
+            
 
 switch = {
     "1" : EasyMethod,
