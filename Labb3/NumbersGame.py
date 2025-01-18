855cd75ab471a467d61415d3e0acbc2588320b28
ikariLain <matheus-torrico@outlook.com>
2025-01-18 12:22:48 +0100

    Add variation_response

--- Labb3/NumbersGame.py
+++ Labb3/NumbersGame.py
@@ -1,19 +1,16 @@
 import random
 
-
 def deafult():
     easy_random = random.randint(1,20)
 
     print("Welcome! I'm thinking on a number from from 1-20")
     print("\nCan you guess it? You have 5 tries")
 
-
-
     tries = 5
 
     for attempt in range(tries):
     
-        user_input = int(input(f"Attempt {attempt + 1}/{tries}: Enter your number"))
+        user_input = int(input(f"Attempt {attempt + 1}/{tries}: Enter your number: "))
     
         if user_input == easy_random:
             print("Congratulations! You guessed it!")
@@ -32,35 +29,51 @@ def deafult():
 
 def pick_your_number():
     while True:
-        start_value = int(input("Enter your start number"))
-        end_value = int(input("Enter your end number"))
-        tries = int(input("How many tries do you need?"))
+        start_value = int(input("\nEnter your start number: "))
+        end_value = int(input("\nEnter your end number: "))
+        tries = int(input("\nHow many tries do you need? "))
         
     
         if end_value < start_value:
-                print("Invalid input, the start nummber is to high")
+                print("\nInvalid input, the start nummber is to high")
                 return 
         else:
             break
         
     rank2_random = random.randint(start_value, end_value)
+    
+    #Depending on the user input get an extra message    
+    def variation_respone(user_input):
+        if (user_input - rank2_random) <=10:
+            return("\nWow! your real close You are close!")
+            
+        elif (user_input - rank2_random) <=20:
+            return("\nYou are within you are getting closer!")
+            
+        elif (user_input - rank2_random) <=60:
+            return("\nYou can do better!  :)  ")
+            
+        else:
+            return("\nYou are not even close!  :/  ")
 
     for attempt  in range(tries):
     
         user_input = int(input(f"Attempt {attempt + 1}/{tries}: Enter your number"))
     
         if user_input == rank2_random:
-            print("Congratulations! You guessed it!")
-            print(f"With only {attempt} tries")
+            print("\nCongratulations! You guessed it!")
+            print(f"\nWith only {attempt} tries")
             break
-    
+            
         elif user_input < rank2_random:
             print("\nToo low! Try again.")
-         
+            print(variation_respone(user_input))
+        
         else:
             print("\nYour number is too high! Try again.")
-        
-# Failed messages
+            print(variation_respone(user_input))
+            
+    # Failed messages
     else:
         print("\nSorry, you didn't guess it. The number was", rank2_random)
         
