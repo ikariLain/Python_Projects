ec5f9dac3c23f1fff5556b694306749555782ce3
ikariLain <matheus-torrico@outlook.com>
2025-01-18 10:06:10 +0100

    Make a module and menu plus a switch for input

--- Labb3/NumbersGame.py
+++ Labb3/NumbersGame.py
@@ -1,29 +1,44 @@
 import random
 
-easy_random = random.randint(1,20)
 
-print("Welcome! I'm thinking on a number from from 1-20")
-print("\nCan you guess it? You have 5 tries")
+def deafult():
+    easy_random = random.randint(1,20)
+
+    print("Welcome! I'm thinking on a number from from 1-20")
+    print("\nCan you guess it? You have 5 tries")
 
 
 
-tries = 5
+    tries = 5
 
-for attempt  in range(tries):
+    for attempt  in range(tries):
     
-    user_input = int(input(f"Attempt {attempt + 1}/{tries}: Enter your number"))
+        user_input = int(input(f"Attempt {attempt + 1}/{tries}: Enter your number"))
     
-    if user_input == easy_random:
-        print("Congratulations! You guessed it!")
-        print(f"With only {tries} tries")
-        break
+        if user_input == easy_random:
+            print("Congratulations! You guessed it!")
+            print(f"With only {tries} tries")
+            break
     
-    elif user_input < easy_random:
-        print("Too low! Try again.")
+        elif user_input < easy_random:
+            print("Too low! Try again.")
          
-    else:
-        print("\nYour number is too high! Try again.")
+        else:
+            print("\nYour number is too high! Try again.")
         
 # Failed messages
-else:
-    print("\nSorry, you didn't guess it. The number was", easy_random)
+    else:
+        print("\nSorry, you didn't guess it. The number was", easy_random)
+        
+        
+        
+        
+        
+def switch(choice):
+    switch = {
+        1: deafult,
+        2: "placeholder"
+    }
+
+print("Welcome to guess the number!")
+choice = int(input("Enter your choice"))
